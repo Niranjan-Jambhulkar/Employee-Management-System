e28586888597584c004158f1172e7e6ce0426eff
Niranjan <niranjan.jambhulkar@outlook.com>
2023-11-25 17:54:56 +0530

    update

--- Functions/Function.c
+++ Functions/Function.c
@@ -20,6 +20,7 @@ typedef struct Employees
     char Date_of_join[20];
     char emp_Address[50];
     char emp_Number[10];
+    char emp_Email[20];
 }Employee;
 // Structures End
 
@@ -30,6 +31,7 @@ void Productivity (Employee [], Product []);
 void Hard_data(Employee [], Product []);
 void Total_Employee (Employee [], int);
 void Employee_Details (Employee [], int);
+void Productivity_Details (Employee [], Product [],int);
 // Functions Declaration End
 
 // Functions Defintion Start
@@ -41,7 +43,10 @@ void Hard_data(Employee emp[], Product prd[]){
     strcpy(emp[0].Date_of_join, "02-05-2020");
     strcpy(emp[0].emp_Address, "Pune, Maharashtra, India");
     strcpy(emp[0].emp_Number, "775609265");
+    strcpy(emp[0].emp_Email, "Raj@outlook.com");
     prd[0].Attendance = 90;
+    prd[0].Productivity = 95;
+    prd[0].Leaves = 8;
 
     emp[1].emp_ID = 2;
     strcpy(emp[1].emp_Name,"Rahul   ");
@@ -50,7 +55,10 @@ void Hard_data(Employee emp[], Product prd[]){
     strcpy(emp[1].Date_of_join, "15-08-2020");
     strcpy(emp[1].emp_Address, "Pune, Maharashtra, India");
     strcpy(emp[1].emp_Number, "762061043");
+    strcpy(emp[1].emp_Email, "Rahul@outlook.com");
     prd[1].Attendance = 60;
+    prd[1].Productivity = 60;
+    prd[1].Leaves = 6;
 
 
     emp[2].emp_ID = 3;
@@ -60,7 +68,10 @@ void Hard_data(Employee emp[], Product prd[]){
     strcpy(emp[2].Date_of_join, "25-04-2022");
     strcpy(emp[2].emp_Address, "Pune, Maharashtra, Pune");
     strcpy(emp[2].emp_Number, "915839194");
+    strcpy(emp[2].emp_Email, "Raju@outlook.com");
     prd[2].Attendance = 80;
+    prd[2].Productivity = 75;
+    prd[2].Leaves = 5;
 }
 
 void Accept (Employee emp[], Product prd[]){
@@ -101,6 +112,7 @@ void Accept (Employee emp[], Product prd[]){
 }
 
 void Display (Employee emp[], Product prd[]){ 
+    printf("\n--------Employee List--------\n");
     for (int i = 0; i < 80; i++)
     {
         printf("-");
@@ -115,11 +127,21 @@ void Display (Employee emp[], Product prd[]){
     { 
         printf("%d       %s        %s            %f        %s\n", emp[i].emp_ID, emp[i].emp_Name, emp[i].Department,emp[i].emp_salary, emp[i].Date_of_join);
     }
-    
+    for (int i = 0; i < 80; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
 }
 
 void Productivity (Employee emp[], Product prd[]){
-    printf("\nID\tName\t\tDepartment\t\tDate of Join\tAttendance\n");
+    printf("\n--------Employee Productivity--------\n");
+    for (int i = 0; i < 80; i++)
+    {
+        printf("-");
+    }
+    
+    printf("\nID\tName\t\tDepartment\t    Date of Join\t  Attendance\n");
     for (int i = 0; i < 80; i++)
     {
         printf("-");
@@ -129,7 +151,11 @@ void Productivity (Employee emp[], Product prd[]){
     { 
         printf("%d       %s        %s            %s            %d per\n", emp[i].emp_ID, emp[i].emp_Name, emp[i].Department, emp[i].Date_of_join, prd[i].Attendance);
     }
-    
+    for (int i = 0; i < 80; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
 }
 
 void Total_Employee (Employee emp[], int total_emp){
@@ -141,6 +167,7 @@ void Total_Employee (Employee emp[], int total_emp){
 }
 
 void Employee_Details (Employee emp[], int a){
+    printf("\n--------Employee Details--------\n");
     if (a<=size)
     {
         for (int i = a-1; i < size;)  // Using a-1 because 1st employee is stored on 0 index, and so on 
@@ -149,8 +176,49 @@ void Employee_Details (Employee emp[], int a){
             {
                 printf("-");
             }
-            printf("\nID: \t\t%d\nName: \t\t%s\nDepartment: \t%s\nDate of Join: \t%s\nAddress: \t%s\nMo. Number: \t%s\n", emp[i].emp_ID, emp[i].emp_Name, emp[i].Department, emp[i].Date_of_join, emp[i].emp_Address, emp[i].emp_Number);
+            printf("\nID: \t\t%d\nName: \t\t%s\nDepartment: \t%s\nDate of Join: \t%s\nSalary: \t%f\nAddress: \t%s\nMo. Number: \t+91 %s\nE-Mail ID: \t%s\n", emp[i].emp_ID, emp[i].emp_Name, emp[i].Department, emp[i].Date_of_join, emp[i].emp_salary, emp[i].emp_Address, emp[i].emp_Number, emp[i].emp_Email);
             for (int i = 0; i < 80; i++)
+            {
+                printf("-");
+            }
+                printf("\n");
+            break;
+        }
+    }
+    else{
+        for (int i = 0; i < 80; i++)
+            {
+                printf("-");
+            }
+        printf("\nNo Employee Found\n");
+        for (int i = 0; i < 80; i++)
+            {
+                printf("-");
+            }
+                printf("\n");
+    }
+}
+
+void Productivity_Details (Employee emp[], Product prd[],int a){
+    if (a<=size)
+    {
+        for (int i = a-1; i < size;)  // Using a-1 because 1st employee is stored on 0 index, and so on 
+        {
+            for (int i = 0; i < 65; i++)
+            {
+                printf("-");
+            }
+            printf("\n| ID \t\t\tName \t\t\tDepartment\t|\n");
+            printf("| %d\t\t\t%s\t\t%s\t|\n",emp[i].emp_ID, emp[i].emp_Name, emp[i].Department);
+            printf("|");
+            for (int i = 0; i < 63; i++)
+            {
+                printf("-");
+            }
+            printf("|\n");
+            printf("| Attendance\t\tProductivity\t\tLeaves\t\t|\n");
+            printf("| %d Percent\t\t%d / 100\t\t%d / 15\t\t|\n",prd[i].Attendance, prd[i].Productivity,prd[i].Leaves);
+            for (int i = 0; i < 65; i++)
             {
                 printf("-");
             }
